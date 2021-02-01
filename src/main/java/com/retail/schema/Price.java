7964b0b74a0403e66eeb68973477a7687ae94b64
Ravi Kiran Moguluri <ravikiran.378@Gmail.Com>
2021-02-01 09:22:53 +0530

    Intial Commit

---
+++ src/main/java/com/retail/schema/Price.java
@@ -0,0 +1,6 @@
+package com.retail.schema;
+
+public class Price {
+    private String currencyCode;
+    private String currencyValue;
+}
---
+++ src/main/java/com/retail/schema/Product.java
@@ -0,0 +1,6 @@
+package com.retail.schema;
+
+public class Product {
+    private String id;
+    private String currentPrice;
+}
